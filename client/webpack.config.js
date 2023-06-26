b345bfe933fb0396e8113065bc17696e5bdbc1ae
pexApred <120075705+pexApred@users.noreply.github.com>
2023-06-26 16:03:27 -0400

    added css loaders to webpack and edited package.json

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -23,7 +23,21 @@ module.exports = () => {
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        }
       ],
     },
   };
