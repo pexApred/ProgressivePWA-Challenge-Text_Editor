45e55c223ac17e122dc846f4e7e81ee9d0a32e5d
pexApred <120075705+pexApred@users.noreply.github.com>
2023-07-01 15:33:14 -0400

    logic added to database.js getDB and putDB

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,46 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => { 
+  console.error('putDb not implemented');
+
+  // Create a connection to the database database and version we want to use.
+  const jateDB = await openDB('jate', 1);
+
+  // Create a new transaction and specify the database and data privileges.
+  const tx = jateDB.transaction('jate', 'readwrite');
+
+  // Open up the desired object store.
+  const store = tx.objectStore('jate');
+
+  // Use the .getAll() method to get all data in the database.
+  await store.add(content);
+
+  await tx.complete;
+  // Get confirmation of the request.
+  console.log('Data added to the database', content);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => { 
+  console.error('getDb not implemented');
+
+  // Create a connection to the database database and version we want to use.
+  const jateDB = await openDB('jate', 1);
+
+  // Create a new transaction and specify the database and data privileges.
+  const tx = jateDB.transaction('jate', 'readonly');
+
+  // Open up the desired object store.
+  const store = tx.objectStore('jate');
+
+  // Use the .getAll() method to get all data in the database.
+  const request = store.getAll();
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
 
 initdb();
