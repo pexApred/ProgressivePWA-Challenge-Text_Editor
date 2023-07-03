b84538d8963517b58a03c5416ce4a4ee192ba6bf
pexApred <120075705+pexApred@users.noreply.github.com>
2023-07-03 13:41:31 -0400

    Implemented click event handlers for install.js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -3,23 +3,29 @@ const butInstall = document.getElementById('buttonInstall');
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
-    event.preventDefault();
-    window.deferredPrompt = event;
-    butInstall.removeAttribute('hidden', false);
+  // event.preventDefault();
+  window.deferredPrompt = event;
+  butInstall.removeAttribute('hidden', false);
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
-  if (window.deferredPrompt) {
-    window.deferredPrompt.prompt();
-    const { outcome } = await window.deferredPrompt.userChoice;
-    console.log(`User response to the install prompt: ${outcome}`);
-    window.deferredPrompt = null;
-    butInstall.setAttribute('hidden', true);
+  const { outcome } = await window.deferredPrompt.userChoice;
+  console.log(`User response to the install prompt: ${outcome}`);
+
+  if (!window.deferredPrompt) {
+    return;
   }
+
+  window.deferredPrompt.prompt();
+
+  window.deferredPrompt = null;
+
+  butInstall.classList.toggle('hidden', true);
 });
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
-    window.deferredPrompt = null;
+  console.log('App install success');
+  window.deferredPrompt = null;
 });
