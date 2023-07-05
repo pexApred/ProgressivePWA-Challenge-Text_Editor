cee607ad8fe774820b5b1b06151bb230dcbcc687
pexApred <120075705+pexApred@users.noreply.github.com>
2023-07-05 10:49:56 -0400

    Improved install.js for better clarity and removed unnecessary lines of code

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,30 +1,29 @@
-const butInstall = document.getElementById('buttonInstall');
-
+const btnInstall = document.getElementById('buttonInstall');
+let deferredPrompt;
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
-  // event.preventDefault();
-  window.deferredPrompt = event;
-  butInstall.classList.toggle('hidden', false);
+  event.preventDefault();
+  deferredPrompt = event;
+  btnInstall.classList.remove('hidden');
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {
-  if (!window.deferredPrompt) {
+btnInstall.addEventListener('click', async () => {
+  if (!deferredPrompt) {
     return;
   }
-  window.deferredPrompt.prompt();
+  deferredPrompt.prompt();
   
-  const { outcome } = await window.deferredPrompt.userChoice;
+  const { outcome } = await deferredPrompt.userChoice;
   console.log(`User response to the install prompt: ${outcome}`);
 
-  window.deferredPrompt = null;
+  deferredPrompt = null;
 
-  butInstall.classList.toggle('hidden', true);
+  btnInstall.classList.add('hidden');
 });
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
   console.log('App install success');
-  window.deferredPrompt = null;
 });
